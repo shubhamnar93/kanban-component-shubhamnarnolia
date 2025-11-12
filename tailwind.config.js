65bccdd4aeafc976a6d163e2ba553c7213a5c3f0
shubhamnar93 <shubhamnarnolia925@gmail.com>
2025-11-12 18:46:32 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend:  {
+      colors: {
+        primary: {
+          50: '#f0f9ff',
+          100: '#e0f2fe',
+          500: '#0ea5e9',
+          600: '#0284c7',
+          700: '#0369a1',
+        },
+        neutral: {
+          50: '#fafafa',
+          100: '#f4f4f5',
+          200: '#e4e4e7',
+          300: '#d4d4d8',
+          700: '#3f3f46',
+          900: '#18181b',
+        },
+        spacing: {
+          18: '4.5rem',
+          88: '22rem',
+        },
+        borderRadius: {
+          'xl': '0.75rem',
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
